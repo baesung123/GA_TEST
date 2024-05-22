c95508e6a167ce209e2800e975d76be5b7e34971
whos-bax <bbak9604@gmail.com>
2024-05-22 12:21:09 +0900

    create-react-app

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ function App() {
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>
-          안녕하세요 배성환입니다.
           {/*Edit <code>src/App.js</code> and save to reload.*/}
+          안녕하세요 <b>배성환</b>입니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
       </header>
     </div>
   );
